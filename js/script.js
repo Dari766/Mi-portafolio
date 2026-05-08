84ff5842c088ada5a76fa987e0dee15f705d0a71
Dariana de la cruz <ortegaadriana1865@gmail.com>
2026-05-08 15:55:03 -0500

    Remove duplicate code in script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
 
     navLinks.classList.toggle("active");
-
 });
 
 /* CERRAR MENU AL HACER CLICK */
